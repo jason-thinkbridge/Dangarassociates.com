ebe36aea2b3ddebc8b36ee1f49b321f00fe7e1c0
Anand Krishnan <anand@thinkbridgesoftware.com>
2015-09-13 17:54:41 -0400

    Stripe Code changes

--- php/stripe.php
+++ php/stripe.php
@@ -1,6 +1,6 @@
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey("pk_live_i16xE5pH0SmIRCMJPIqmms6R);
+\Stripe\Stripe::setApiKey("pk_test_wAHLBj9ujy5r6x3T9xdBFCkn");
 
 // Get the credit card details submitted by the form
 $token = $_POST['stripeToken'];
@@ -8,10 +8,10 @@ $token = $_POST['stripeToken'];
 // Create the charge on Stripe's servers - this will charge the user's card
 try {
 $charge = \Stripe\Charge::create(array(
-  "amount" => 1000, // amount in cents, again
+  "amount" => $_POST('amount')*100, // amount in cents, again
   "currency" => "usd",
   "source" => $token,
-  "description" => "Example charge")
+  "description" => $_POST('invoice-number')
 );
 } catch(\Stripe\Error\Card $e) {
   // The card has been declined
