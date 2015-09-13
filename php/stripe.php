02f00985b97e7ed07ff06f5a7e50543d6dd9753e
Anand Krishnan <anand@thinkbridgesoftware.com>
2015-09-13 19:16:11 -0400

    stripe commit

--- php/stripe.php
+++ php/stripe.php
@@ -1,3 +1,4 @@
+<?php
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here https://dashboard.stripe.com/account/apikeys
 \Stripe\Stripe::setApiKey("pk_test_wAHLBj9ujy5r6x3T9xdBFCkn");
@@ -12,7 +13,8 @@ $charge = \Stripe\Charge::create(array(
   "currency" => "usd",
   "source" => $token,
   "description" => $_POST('invoice-number')
-);
+));
 } catch(\Stripe\Error\Card $e) {
   // The card has been declined
 }
+?>
